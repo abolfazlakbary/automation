e61c73bfb7f551615e4a41d5123528affd5275ad
Abolfazl <abolfazl.akbary132@gmail.com>
2024-09-27 16:56:47 +0330

    commenting unfinished routes

--- automation/app/api/v1/tools/recon/router.py
+++ automation/app/api/v1/tools/recon/router.py
@@ -8,19 +8,19 @@ recon_router = APIRouter(prefix="/recon", tags=["Recon"])
 controller = ReconController()
 
 
-@recon_router.get(
-    "/subenum/info",
-    summary="Get SubEnum info"
-)
-async def func_get_subenum_info():
-    data = await controller.get_subenum_info()
-    return SuccessResponse.show(data=data)
+# @recon_router.get(
+#     "/subenum/info",
+#     summary="Get SubEnum info"
+# )
+# async def func_get_subenum_info():
+#     data = await controller.get_subenum_info()
+#     return SuccessResponse.show(data=data)
 
 
-@recon_router.get(
-    "/subenum/search",
-    summary="Search a domain with SubEnum"
-)
-async def func_search_with_subenum(domain: str):
-    data = await controller.subenum_search(domain)
-    return SuccessResponse.show(data=data)
+# @recon_router.get(
+#     "/subenum/search",
+#     summary="Search a domain with SubEnum"
+# )
+# async def func_search_with_subenum(domain: str):
+#     data = await controller.subenum_search(domain)
+#     return SuccessResponse.show(data=data)
